247097885f65e8e88382029d12fb2afaf01f8870
“Cenek <cenek.pitel@deltatre.com>
2021-05-19 00:22:34 +0200

    Rework registration.js accprding to Best Practises

--- cypress/integration/Tests/registration.js
+++ cypress/integration/Tests/registration.js
@@ -17,53 +17,34 @@ const clickCreateAnAccountButton = () => {
     cy.get('button[name=SubmitCreate]').click()
 }
 
-describe('Registration test - invalid email', function () {
+describe('Registration test', function () {
 
     // Get to the Sign in / registration page
-    before(() => {
+    beforeEach(() => {
         visitWebsite()
         cy.get('.login').click()
         cy.get('.page-subheading').contains('Create an account', {matchCase: false})
     })
  
-    it('Enters invalid email', function () {
+    it('Enters invalid email and check error message', function () {
         enterEmail(testData.invalidEmail)
-    })
-
-    it('Clicks Create an account button', function() {
         clickCreateAnAccountButton()
+        cy.get('.alert-danger')
+        cy.get('#create_account_error').contains('li', 'Invalid email address.')
     })
-    
-    it('Returns invalid email address error message', () => {
-        cy.get('#create_account_error')
-            .contains('li', 'Invalid email address.')
-    })
-})
-
-describe('Registration test - valid email', function () {
 
     // Get to the Sign in / registration page
-    before(() => {
-         cy.wrap(randomEmail).as('randomEmail')
-         visitWebsite()
-         cy.get('.login').click()
-         cy.get('.page-subheading').contains('Create an account', {matchCase: false})
-    })
-
-    it('Enters valid email', function () {
+    it('Fill the registration form with valid data and check user is registered', function () {
+        cy.wrap(randomEmail).as('randomEmail')
         enterEmail(randomEmail)
-    })
-    it('Clicks Create an account button', function() {
         clickCreateAnAccountButton()
-    })
-    it('Input personal informations', function () {
+        // Input personal informations
         cy.get('.account_creation').contains('Your personal information').parent('div').within(() => {
             cy.get('input[id=customer_firstname]').type(testData.firstname).should('have.value', testData.firstname)
             cy.get('input[id=customer_lastname]').type(testData.lastname).should('have.value', testData.lastname)
             cy.get('input[id=passwd]').type(testData.validPassword)
         })
-    })
-    it('Input address', function () {
+        // Input address
         cy.get('.account_creation').contains('Your address').parent('div').within(() => {
             cy.get('input[id=address1]').type(testData.address.street).should('have.value', testData.address.street)
             cy.get('input[id=city]').type(testData.address.city).should('have.value', testData.address.city)
@@ -72,12 +53,10 @@ describe('Registration test - valid email', function () {
             cy.get('input[id=phone_mobile]').type(testData.phoneNumber).should('have.value', testData.phoneNumber)
             cy.get('input[id=alias]').clear().type(testData.addressAlias).should('have.value', testData.addressAlias)
         })
-    })
-    it('Clicks on register button', function () {
+        // Click registration button
         cy.get('button[name=submitAccount]').click()
-    })
-    it('Checks user is signed in', function () {
         cy.get('.header_user_info').find('a').contains('Sign out').should('have.attr', 'href')
         cy.get('.header_user_info').find('a').children().contains(`${testData.firstname} ${testData.lastname}`).parent().should('have.attr', 'href')
+        
     })
 })
